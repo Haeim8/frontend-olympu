6afa9c946b1d4c878c535283a21900a5c6dfa8c4
Haeim8 <zifou.rail.91290@gmail.com>
2024-10-07 04:05:09 +0200

    correct

--- app/page.js
+++ app/page.js
@@ -4,36 +4,18 @@ import { useState, useEffect } from 'react';
 import { useAccount } from 'wagmi';
 import Home from '@/components/home';
 import AppInterface from '@/components/app-interface';
-import { db } from '@/lib/firebase/firebase';
-import { doc, getDoc } from 'firebase/firestore';
 
 export default function Page() {
-  const { address, isConnected } = useAccount();
+  const { isConnected } = useAccount();
   const [showInterface, setShowInterface] = useState(false);
-  const [isRegistered, setIsRegistered] = useState(false);
 
   useEffect(() => {
-    const checkRegistration = async () => {
-      if (isConnected && address) {
-        try {
-          const userRef = doc(db, 'users', address);
-          const userDoc = await getDoc(userRef);
-          if (userDoc.exists()) {
-            setIsRegistered(true);
-            setShowInterface(true);
-          } else {
-            setIsRegistered(false);
-            setShowInterface(false);
-          }
-        } catch (error) {
-          console.error("Erreur lors de la vérification de l'inscription :", error);
-        }
-      } else {
-        setShowInterface(false);
-      }
-    };
-    checkRegistration();
-  }, [isConnected, address]);
+    if (isConnected) {
+      setShowInterface(true);
+    } else {
+      setShowInterface(false);
+    }
+  }, [isConnected]);
 
   return (
     <div className="min-h-screen bg-background">
