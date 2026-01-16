bcac4dc05c445698b95bd804b39edbe289f8bec9
Votre djangode <zifou.rail.91290@gmail.Com>
2026-01-16 19:45:32 +0100

    fix provider

--- frontend/app/ClientProviders.jsx
+++ frontend/app/ClientProviders.jsx
@@ -1,10 +1,27 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'next-themes';
 import ThirdwebProviderWrapper from "./ThirdwebProviderWrapper";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { ToastProvider } from "@/contexts/ToastContext";
+
+// Composant pour initialiser le SDK Farcaster Mini App
+function FarcasterSDKInit() {
+  useEffect(() => {
+    const initFarcaster = async () => {
+      try {
+        const { sdk } = await import('@farcaster/miniapp-sdk');
+        await sdk.actions.ready();
+        console.log('[Farcaster] Mini App SDK Ready');
+      } catch (e) {
+        // Pas dans un contexte Farcaster (navigateur normal)
+      }
+    };
+    initFarcaster();
+  }, []);
+  return null;
+}
 
 export default function ClientProviders({ children }) {
   return (
@@ -12,6 +29,7 @@ export default function ClientProviders({ children }) {
       <LanguageProvider>
         <ToastProvider>
           <ThirdwebProviderWrapper>
+            <FarcasterSDKInit />
             {children}
           </ThirdwebProviderWrapper>
         </ToastProvider>
